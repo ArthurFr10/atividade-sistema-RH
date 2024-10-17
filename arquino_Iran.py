722b0d6e7351030a3145508f7fd0e237c14dda94
iranferreir <iranferreiradossantos123@gmail.com>
2024-10-17 19:49:08 +0000

    salva

--- arquino_Iran.py
+++ arquino_Iran.py
@@ -16,3 +16,17 @@ def menu():
 6 - Sair do sistema
 """)
       
+def salvar_funcionario(funcionario):
+    pass
+
+def listar_todos_funcionarios():
+    pass
+
+def pesquisar_um_funcionario(funcionario):
+    pass
+
+def atualizar_funcionario(funcionario):
+    pass
+
+def excluir_funcionario(funcionario):
+    pass
